55a11550404a60837a551ee450b552bb32740da8
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2018-05-19 23:23:27 +0200

    Song and progressBar logic

--- songs.js
+++ songs.js
@@ -1,10 +1,19 @@
-var playingSong = "";
+var playingSong = null;
 
 var songs = document.getElementsByClassName("song");
 
 for (i = 0; i < songs.length; i++) {
     songs[i].onclick = function () {
-        pauseOrPlay(this);
+        if (playingSong == this || playingSong == null) {
+            if (playingSong == null) playingSong = this;
+            pauseOrPlay(this);
+            setProgressBarOfSong(this);
+        } else {
+            resetStateOfSong(playingSong);
+            playingSong = this;
+            pauseOrPlay(this);
+            setProgressBarOfSong(this);
+        }
     }
 }
 
@@ -15,7 +24,7 @@ function pauseOrPlay(song) {
 
     if (audio.paused) {
         play(audio, pauseIcon, playIcon);
-        song.style.color="red";
+        song.style.color = "red";
     } else {
         pause(audio, pauseIcon, playIcon);
     }
@@ -35,5 +44,21 @@ function pause(audio, pause, play) {
 
 
 function setProgressBarOfSong(song) {
-    
+    var audio = song.getElementsByTagName("audio")[0];
+    var progressBar = song.getElementsByClassName("progressBar")[0];
+    if (audio.paused) {
+
+    } else {
+        var songPercentage = audio.currentTime / audio.duration * 100;
+        progressBar.style.width = songPercentage + "%";
+        setTimeout(function () { setProgressBarOfSong(song) }, 1);
+    }
+}
+
+function resetStateOfSong(song) {
+    song.style.color = "black";
+    song.getElementsByTagName("audio")[0].load();
+    song.getElementsByClassName("progressBar")[0].style.width = "0";
+    song.getElementsByClassName("mediaButtons")[0].classList.remove("hidden");
+    song.getElementsByClassName("mediaButtons")[1].classList.add("hidden");
 }
